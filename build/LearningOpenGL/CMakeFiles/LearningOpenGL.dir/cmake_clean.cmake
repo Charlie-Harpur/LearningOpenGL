dbce36b2681dfe88888153bf8b146cd9d992865d
Charlie_H <c.harpur@hotmail.com>
2023-01-19 16:21:52 -0500

    Created template for project

---
+++ build/LearningOpenGL/CMakeFiles/LearningOpenGL.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LearningOpenGL.dir/LearningOpenGL.cpp.obj"
+  "CMakeFiles/LearningOpenGL.dir/LearningOpenGL.cpp.obj.d"
+  "LearningOpenGL.exe"
+  "LearningOpenGL.exe.manifest"
+  "LearningOpenGL.pdb"
+  "libLearningOpenGL.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LearningOpenGL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
